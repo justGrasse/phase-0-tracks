76e8740228347a596de6cc8f7b8bc715ba0a0e08
Justin Grasse <just.grasse@gmail.com>
2016-10-25 11:38:55 -0700

    update vampires.rb for release 1, DBC  4.4

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "Thank you for your interest in Werewolf Inc."
+puts "Please complete the following survey:"
+puts "What is your name:"
+name = gets.chomp
+puts = "How old are you?"
+age = gets.to_i
+puts = "What year were you born?"
+birth_year = gets.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+wants_garlic = puts.chomp
+puts "Would you like to enroll in the company's health insurance?"
+
