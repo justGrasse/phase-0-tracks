16854d349a05369a76c26ff03bfff997cfcbb9a0
Justin Grasse <just.grasse@gmail.com>
2016-10-25 18:51:24 -0700

    add requests for hamster info on hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+puts "Tell us about that hamster!!!"
+puts "Name?"
+name = gets.chomp
+puts "Volume? (1-10)"
+volume = gets.to_i
+puts "Fur color?"
+fur_color = gets.chomp
+puts "Good candidate for adoption? AKA Is it docile? (y/n)"
+is_docile = gets.chomp
+puts "Estimated age?"
+age = nil
+age = gets.to_i
+
