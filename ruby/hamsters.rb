65e289401c030b5e6f28662f7f84e47b6d7c96e2
Justin Grasse <just.grasse@gmail.com>
2016-10-25 19:03:44 -0700

    add code to convert strings to specific types of data on hamsters.rb

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,21 @@
+
+def to_boo(str)
+	return true if str == "true"
+	return false if str == "false"
+	return nil
+end
+
 puts "Tell us about that hamster!!!"
 puts "Name?"
 name = gets.chomp
 puts "Volume? (1-10)"
 volume = gets.to_i
+# puts "The volume is #{volume}"
 puts "Fur color?"
 fur_color = gets.chomp
-puts "Good candidate for adoption? AKA Is it docile? (y/n)"
-is_docile = gets.chomp
+puts "This is a good candidate for adoption. AKA It is docile? (true/false)"
+is_docile = to_boo(gets.chomp)
+# puts "The user chose #{is_docile}"
 puts "Estimated age?"
 age = nil
 age = gets.to_i
-
