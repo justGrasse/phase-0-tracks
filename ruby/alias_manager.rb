9c0f89583732e47b9171e0b9957dace420b8fcef
Justin Grasse <just.grasse@gmail.com>
2016-11-01 16:28:40 -0700

    pseudocode the methods for Alias program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,24 @@
+# DOMAIN LOGIC
+
+# getAlias('name')
+
+# nextVowel [METHOD]
+# vowels = "aeiou".split('')
+
+# nextConsonant [METHOD]
+
+
+# DRIVER CODE
+
+# Get name from user
+
+# Swap the first and last name
+
+# Downcase and convert string to array in order to map!
+
+# use map! to loop through each character of array
+
+	# if vowel, provide next vowel [METHOD]
+
+	# if consonant, provide next consonant [CONSONANT]
+
