798cf10f211b9f5e9203754638c1922381b8a5ff
Justin Grasse <just.grasse@gmail.com>
2016-11-01 17:02:33 -0700

    create getAlias method for Alias Creator

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,9 +1,65 @@
 # DOMAIN LOGIC
 
 # getAlias('name')
+def getAlias(name)
+	# Downcase + Swap the first and last name
+	aliasName = name.downcase
+	aliasName = aliasName.split(' ')
+	aliasName.reverse!
+
+	# Convert string to array in order to map!
+	aliasName = aliasName.join(' ')
+	aliasName = aliasName.split('')
+	
+	# conce
+	# aliasName = nextLetters(aliasName)
+	aliasName = aliasName.join('')
+	aliasName = aliasName.split(' ')
+	aliasName.map! { |name|
+		name.capitalize
+	}
+	aliasName = aliasName.join(' ')
+end
+
+# nextLetters('aliasArray')
+
+def nextLetters(aliasArray)
+	vowels = "aeiou".split('')
+	consonants = "bcdfghjklmnpqrstvwxyz".split('')
+
+	# use map! to loop through each character of array
+	aliasArray.map! {|char|
+		# if vowel, provide next vowel [METHOD]
+		if vowels.include? char
+			vowels[(vowels.index(char) + 1) % 5]
+
+		# if consonant, provide next consonant [CONSONANT]
+		elsif consonants.include? char
+			consonants[(consonants.index(char) + 1) % 19]
+		end
+	}	
+
+end
+
+
+
+	# use map! to loop through each character of array
+	#aliasName.map! {|char|
+
+		# if vowel, provide next vowel [METHOD]
+
+
+		# if consonant, provide next consonant [CONSONANT]
+
+	#}
 
 # nextVowel [METHOD]
-# vowels = "aeiou".split('')
+def nextVowel(char)
+	vowels = "aeiou".split('')
+
+
+
+end
 
 # nextConsonant [METHOD]
 
@@ -12,13 +68,4 @@
 
 # Get name from user
 
-# Swap the first and last name
-
-# Downcase and convert string to array in order to map!
-
-# use map! to loop through each character of array
-
-	# if vowel, provide next vowel [METHOD]
-
-	# if consonant, provide next consonant [CONSONANT]
-
+puts getAlias("Justin Grasse")
