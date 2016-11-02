55d57d9304206ca64894cea846427a2646fe196d
Justin Grasse <just.grasse@gmail.com>
2016-11-01 17:22:18 -0700

    write and verify getAlias() and nextLetters() methods to create alias name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,71 +1,52 @@
 # DOMAIN LOGIC
 
-# getAlias('name')
+# getAlias('name'): Converts a name into an Alias (per DBC 5.5)
 def getAlias(name)
 	# Downcase + Swap the first and last name
 	aliasName = name.downcase
 	aliasName = aliasName.split(' ')
 	aliasName.reverse!
 
-	# Convert string to array in order to map!
+	# Convert string to char array in order to call nextLetters()
 	aliasName = aliasName.join(' ')
 	aliasName = aliasName.split('')
+	aliasName = nextLetters(aliasName)
 	
-	# conce
-	# aliasName = nextLetters(aliasName)
+	# Capitalize each name
 	aliasName = aliasName.join('')
 	aliasName = aliasName.split(' ')
-	aliasName.map! { |name|
-		name.capitalize
-	}
+	aliasName.map! { |name| name.capitalize}
 	aliasName = aliasName.join(' ')
 end
 
-# nextLetters('aliasArray')
+# nextLetters('aliasArray'): Shifts each letter to next vowel/consonant
 
 def nextLetters(aliasArray)
-	vowels = "aeiou".split('')
-	consonants = "bcdfghjklmnpqrstvwxyz".split('')
+	vowels = 'aeiou'.split('')
+	consonants = 'bcdfghjklmnpqrstvwxyz'.split('')
 
 	# use map! to loop through each character of array
 	aliasArray.map! {|char|
+		
 		# if vowel, provide next vowel [METHOD]
 		if vowels.include? char
 			vowels[(vowels.index(char) + 1) % 5]
 
 		# if consonant, provide next consonant [CONSONANT]
 		elsif consonants.include? char
-			consonants[(consonants.index(char) + 1) % 19]
+			consonants[(consonants.index(char) + 1) % 21]
+		
+		else
+			' '
 		end
 	}	
-
 end
-
-
-
-	# use map! to loop through each character of array
-	#aliasName.map! {|char|
-
-		# if vowel, provide next vowel [METHOD]
-
-
-		# if consonant, provide next consonant [CONSONANT]
-
-	#}
-
-# nextVowel [METHOD]
-def nextVowel(char)
-	vowels = "aeiou".split('')
-
-
-
-end
-
-# nextConsonant [METHOD]
-
 
 # DRIVER CODE
 
 # Get name from user
 
-puts getAlias("Justin Grasse")
+puts getAlias('Justin Grasse')
+puts 'Hsetti Katvop'
+puts getAlias('Felicia Torres')
+puts 'Vussit Gimodoe'
