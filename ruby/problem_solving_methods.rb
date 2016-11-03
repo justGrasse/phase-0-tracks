eb516b15fbb1bf9f25f6d04202aa804c2e33fea1
Justin Grasse <just.grasse@gmail.com>
2016-11-03 15:18:09 -0700

    create search_array to mimic index method for DBC 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,7 +1,14 @@
 # DOMAIN LOGIC
 
 def search_array(arr,num)
-
+	index = 0
+	while index < arr.length
+		if arr[index] == num
+			return index
+		end
+		index += 1
+	end
+	nil
 end
 
 
@@ -10,5 +17,37 @@ end
 
 # TEST CODE
 arr = [42,89,23,1]
-p search_array(arr,1) + " should print '3'"
-p search_array(arr,24) + "should print 'nil'"
+p search_array(arr,1) 
+puts "should print '3'"
+p search_array(arr,24) 
+puts "should print 'nil'"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def capAll (str)
+# 	str = str.split(' ')
+# 	str.map! { |str|
+# 		str.capitalize!
+# 	}
+# 	str.join(' ')
+# end
+
+# puts capAll("justin grasse")
+
