492ac82d66fec736fb9ef57fc34c7f5e5313dda6
Justin Grasse <just.grasse@gmail.com>
2016-11-03 13:31:17 -0700

    create template for problem_solving_methods.rb with test cases

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,14 @@
+# DOMAIN LOGIC
+
+def search_array(arr,num)
+
+end
+
+
+# DRIVER CODE
+
+
+# TEST CODE
+arr = [42,89,23,1]
+p search_array(arr,1) + " should print '3'"
+p search_array(arr,24) + "should print 'nil'"
