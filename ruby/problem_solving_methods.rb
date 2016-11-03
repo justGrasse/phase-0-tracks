1de06d93669d211442e5ed9e975c890a906f2a9d
Justin Grasse <just.grasse@gmail.com>
2016-11-03 15:30:55 -0700

    create fib sequence generator for DBC 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -11,19 +11,43 @@ def search_array(arr,num)
 	nil
 end
 
+def fib(num)
+	if num < 1
+		return []
+	elsif num == 1
+		return [0]	
+	else
+		fib_array = [0,1]
+	end
+
+	(num - 2).times do
+		fib_array.push(fib_array[-1] + fib_array[-2])
+	end
+	fib_array	
+end
+
+
 
 # DRIVER CODE
 
 
 # TEST CODE
-arr = [42,89,23,1]
-p search_array(arr,1) 
-puts "should print '3'"
-p search_array(arr,24) 
-puts "should print 'nil'"
 
+# arr = [42,89,23,1]
+# p search_array(arr,1) 
+# puts "should print '3'"
+# p search_array(arr,24) 
+# puts "should print 'nil'"
 
-
+# p fib(0)
+# p fib(1)
+puts "fib(2):"
+p fib(2)
+puts "fib(6):"
+p fib(6)
+puts "test fib(100):"
+puts fib(100).last == 218922995834555169026
+puts fib(100).last
 
 
 
