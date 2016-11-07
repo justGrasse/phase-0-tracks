abf8648ebd0656eba7e3d59df0f8157b3004c8d0
Justin Grasse <just.grasse@gmail.com>
2016-11-07 15:07:51 -0800

    convert old module to a mixin for two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,44 @@
+# DOMAIN LOGIC
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " =["
 	end
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!" + " =]"
 	end
 end
 
-message = "Hello World"
+class Orangutan
+	include Shout
+end
 
-puts Shout.yell_angrily(message)
-puts Shout.yell_happily(message)
+class Klutz
+	include Shout
+end
+
+
+# DRIVER CODE
+
+donny = Orangutan.new
+puts donny.yell_happily("OooOoOhh OoooOHhh Ah ahhHAHHH")
+
+herbert = Klutz.new
+puts herbert.yell_angrily("Oh no, that's a banana pe...")
+
+# OLD MODULE
+
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " =["
+# 	end
+# 	def self.yell_happily(words)
+# 		words + "!!!" + " =]"
+# 	end
+# end
+
+# message = "Hello World"
+
+# puts Shout.yell_angrily(message)
+# puts Shout.yell_happily(message)
+
