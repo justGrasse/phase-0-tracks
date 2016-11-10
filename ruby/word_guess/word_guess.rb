a5a68b864002a0d22d33e8303f070e075accf564
Justin Grasse <just.grasse@gmail.com>
2016-11-09 17:43:17 -0800

    add pseudocode for the word game

---
+++ ruby/word_guess/word_guess.rb
@@ -0,0 +1,21 @@
+# DOMAIN LOGIC
+
+
+
+
+
+
+
+
+
+# DRIVER LOGIC
+
+# Get the WORD from user #1
+
+# Check if the game is over, otherwise
+
+	# Display the BOARD
+
+	# Get GUESS from user #2
+
+# Display CONGRATS or FAILURE message
