f2cd826645f8ed858b908fd8d2e14a121e5ebb50
Justin Grasse <just.grasse@gmail.com>
2016-11-09 17:25:34 -0800

    create todo_list methods for add_item, delete_item, get_item

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -10,32 +10,16 @@ class TodoList
     @chores  
   end
 
+  def add_item(new_item)
+    @chores.push(new_item)
+  end
+
+  def delete_item(bad_item)
+    @chores.delete(bad_item)
+  end
+
+  def get_item(input_index)
+    @chores[input_index]
+  end
+
 end
-
-
-
-
-
-# require_relative 'todo_list'
-
-# describe TodoList do
-#   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
-
-#   it "stores the list items given on initialization" do
-#     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
-#   end
-
-#   it "adds an item to the list" do
-#     list.add_item("mop")
-#     expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
-#   end
-
-#   it "deletes an item" do
-#     list.delete_item("do the dishes")
-#     expect(list.get_items).to eq ["mow the lawn"]
-#   end
-
-#   it "retrieves an item by index" do
-#     expect(list.get_item(0)).to eq "do the dishes"
-#   end
-# end
