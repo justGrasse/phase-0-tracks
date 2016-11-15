453305b15d8f9dfeeb213bbe091eafbf47458774
Justin Grasse <just.grasse@gmail.com>
2016-11-15 11:52:14 -0800

    finish code for random string generator

--- js/algos.js
+++ js/algos.js
@@ -32,21 +32,36 @@ function shareProp(obj1, obj2){
 }
 
 // GENERATE A RANDOM ARRAY OF RANDOM STRINGS
+
+// Random integer function
+function randInt(num){
+	// Return a random number from 1 to 10
+	return Math.floor(Math.random() * num + 1);
+}
+
 // Declare a function to build a random string array
-function shareProp(num){
+function randArr(num){
 	// Initialize a random array <empty>
 	var randomArr = [];
+	// Declare the alphabetALPHABET!
+	var alpha = "abcdefghijklmnopqrstuvwxyz";
+	alpha += alpha.toUpperCase();
 	// Iterate n times per input
-	for (i = 0; i < num; i++){
+	for (var i = 0; i < num; i++){
 		// Choose a random number between 1 and 10
+		randomNum = randInt(10);
 		//Initialize a string <empty>
+		var newStr = "";
 			//Iterate random n times
+			for (var j = 0; j < randomNum; j++){
 				// Push a random letter to the random string
+				newStr += alpha[randInt(alpha.length)-1];
+			}
 		// Push the random assembled string to the random array
-
-		randomArr.push();
+		randomArr.push(newStr);
 	}
 	// Return the random array
+	return randomArr;
 }
 
 
@@ -63,7 +78,7 @@ console.log(longName);
 
 var electionArr = ["the first election",
 	"Election of 1886",
-	"The THANK GOD it's FINALLY IS OVER ELECTION of 20 FRIGGN 16"];
+	"The THANK GOD it's FINALLY IS OVER ELECTION of 20-FRIGGN'-16"];
 longElection = grabLongString(electionArr);
 console.log(longElection);
 
@@ -74,3 +89,6 @@ console.log("This should be true: " + shareProp(user1,user2));
 var movie1 = {title: "Halloween", genre: "horror", hasSeen: true}; 
 var movie2 = {title: "Boo: A Madea Halloween", genre: "comedy", hasSeen: false};
 console.log("This should be false: " + shareProp(movie1,movie2));
+
+console.log(randArr(3));
+console.log(randArr(7));
