01a74688347e3c9f541a334e31697ad3b8a01ae5
Justin Grasse <just.grasse@gmail.com>
2016-11-14 21:20:09 -0800

    pseudocode a function to grab the longest string of an array

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// DOMAIN LOGIC //
+
+// Declare function to grab the longest string
+	// Declare the longest string to find <empty>
+	// Iterate through each element of the array
+		// If the string is longer than the longest, change it!
+	// Return the longest string
+
+
+
+// DRIVER CODE //
+
