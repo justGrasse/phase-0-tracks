475724a153f4c272f1baec02c6eec458b9617b5d
Justin Grasse <just.grasse@gmail.com>
2016-11-27 16:34:41 -0800

    add method to print activities list

--- databases/daily_todo/daily_todo.rb
+++ databases/daily_todo/daily_todo.rb
@@ -10,6 +10,15 @@ require 'faker'
 db = SQLite3::Database.new("todo.db")
 db.results_as_hash = true
 
+
+# create SQL command for activity table
+create_todo_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS activities(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255)
+	) 
+SQL
+
 # create SQL command for todo table
 create_todo_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS todo(
@@ -17,14 +26,6 @@ create_todo_table_cmd = <<-SQL
 		activity_id INT,
 		done BOOLEAN,
 		FOREIGN KEY (activity_id) REFERENCES activities(id)
-	) 
-SQL
-
-# create SQL command for activity table
-create_todo_table_cmd = <<-SQL
-	CREATE TABLE IF NOT EXISTS activities(
-		id INTEGER PRIMARY KEY,
-		name VARCHAR(255)
 	) 
 SQL
 
@@ -52,7 +53,7 @@ end
 
 # create method to add activities
 def add_activity(db, name)
-	db.execute("INSERT INTO activities (name) VALUES (name)", [name])
+	db.execute("INSERT INTO activities (name) VALUES (?)", [name])
 end
 
 # create method to complete activity
@@ -77,6 +78,15 @@ def print_full_list(db)
 	todo_list.each { |act| puts "#{act['activity']} - COMPLETE" }
 end
 
+# create method to print activities
+def print_activities(db)
+	todo_list = db.execute("SELECT * FROM activities")
+	puts "\nHere are the available activities:"
+	puts '*~'*20+'*'
+	# is_included = to show if the activity is in the todolist
+	todo_list.each { |act| puts "#{act['id']} - #{act['name']}" }
+end
+
 # create method to wipe table clean (always double checks!)
 def delete_table(db)
 	puts ('Are you sure you want to wipe the table? (y/n)')
@@ -95,10 +105,11 @@ check_in(db)
 
 # sets a default list of activities:
 db.execute("DELETE FROM todo")
+db.execute("DELETE FROM activities")
 add_activity(db, "Ab Rolls")
 add_activity(db, "Push Ups")
 add_activity(db, "CodeWars Challenge")
-
+print_activities(db)
 
 # print today's to-do list:
 print_todo_list(db)
