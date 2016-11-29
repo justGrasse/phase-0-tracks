3491714e715ab0571a928410a98e5adf8731e4f0
Justin Grasse <just.grasse@gmail.com>
2016-11-28 20:07:15 -0800

    add .js file changing background color and first image border

---
+++ web_dev/client_side_js/client_side.js
@@ -0,0 +1,9 @@
+var html = document.getElementsByTagName("html");
+html = html[0];
+html.style.backgroundColor = "black";
+
+var image = document.getElementsByTagName("img");
+image = image[0];
+image.style.borderRadius = "20px";
+image.style.borderWidth = "8px";
+
