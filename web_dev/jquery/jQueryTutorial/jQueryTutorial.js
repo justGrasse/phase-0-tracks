e58f3a497cdeeacec53b500011f3e7f11fcef4d4
Justin Grasse <just.grasse@gmail.com>
2016-11-29 14:37:18 -0800

    make buttons appear

---
+++ web_dev/jquery/jQueryTutorial/jQueryTutorial.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$(".buttonBox").fadeIn('slow');
+	$(".buttonBox").css('display','block');
+	$(".buttonBox p").fadeIn('slow');
+
+	$("#greenButton").hover(function(){
+		
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
