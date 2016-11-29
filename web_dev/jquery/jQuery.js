95193422209664707ef199af0fc7279c701b6495
Justin Grasse <just.grasse@gmail.com>
2016-11-29 11:42:42 -0800

    add rainbow fadeToggle

--- web_dev/jquery/jQuery.js
+++ web_dev/jquery/jQuery.js
@@ -11,13 +11,26 @@ $(document).ready(function(){
 	// TEST THE ELEMENT SELECTOR $("em") and EVENT METHOD .click
 	// ---> Click the EMPHASIS to make the aside disappear
 	$("em").click(function(){
-		$("aside").hide();
+		$("aside").toggle(500);
 	});
 
 	// Similarly you could use the id selector: $("#id") 
 	// ...or the class selector: $(".class")
 	// More examples of selectors: http://www.w3schools.com/jquery/jquery_selectors.asp
 	// More examples of events: http://www.w3schools.com/jquery/jquery_events.asp
+
+	// jQuery Effects:
+	// show(), hide(), toggle()
+	// 
+	$("#boxButton").click(function(){
+		$("#box1").fadeToggle(500).css("display","inline-block");
+		$("#box2").fadeToggle(1000).css("display","inline-block");
+		$("#box3").fadeToggle(1500).css("display","inline-block");
+		$("#box4").fadeToggle(2000).css("display","inline-block");
+		$("#box5").fadeToggle(2500).css("display","inline-block");
+		$("#box6").fadeToggle(3000).css("display","inline-block");
+		$("#box7").fadeToggle(3500).css("display","inline-block");
+	});
 
 
 
